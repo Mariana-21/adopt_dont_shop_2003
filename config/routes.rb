b1df593a2d319eccd497e8fb9d068f63ff4972b8
Mariana Cid <cidmariana5@gmail.com>
2020-05-05 11:14:22 -0600

    GET POST routes created

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/shelters/new', to: "shelters#new"
   get '/shelters/:shelter_id', to: "shelters#show"
   post '/shelters', to: "shelters#create"
+  get '/shelters/:shelter_id/edit', to: "shelters#edit"
+  patch '/shelters/:shelter_id', to: "shelters#update"
 end
