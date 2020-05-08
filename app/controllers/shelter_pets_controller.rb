a9798539b5e61da438ce84bbab8174b55de74b00
Mariana Cid <cidmariana5@gmail.com>
2020-05-07 21:56:00 -0600

    new/create methods created for pets

--- app/controllers/shelter_pets_controller.rb
+++ app/controllers/shelter_pets_controller.rb
@@ -2,4 +2,23 @@ class ShelterPetsController < ApplicationController
   def index
     @shelter_pet = Shelter.find(params[:shelter_id])
   end 
+
+  # def new
+  #   @shelter_pet = Shelter.find(params[:shelter_id])
+  # end 
+
+  # def create
+  #   shelter = Shelter.find(params[:shelter_id])
+  #   pet = Pet.create(pet_params)
+  #   pet.adoptable_status = "Adoptable"
+
+  #   pet.save
+  #   redirect_to "/shelters/#{shelter.id}/pets"
+  # end 
+
+  # private
+
+  # def pet_params
+  #   params.permit(:name, :description, :age, :sex, :image)
+  # end
 end
